ba4d6ce729136e23cd6511d39e638e6f75076aa5
Fabiano Nalin <fnalin@msn.com>
2018-01-17 13:43:36 -0200

    Add log4net

---
+++ log4net/DemoConsoleApp/DemoConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace DemoConsoleApp
+{
+    //logging.apache.org/log4net/release/manual/configuration.html
+    //docs.microsoft.com/pt-br/aspnet/core/fundamentals/logging/?tabs=aspnetcore2x
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+
+            var logger = LogManager.GetLogger(typeof(Program));
+            logger.Error($"Hello World {new Random().Next(10)}!");
+
+            Console.WriteLine("FIM");
+        }
+    }
+}
